b7a9edc1a6b9b19bce171c686e2f678e37cdae7c
Mustafa Dokumaci <mustafa@foraa.com>
2016-07-30 02:01:25 +0200

    Test get package.json

--- test/now.js
+++ test/now.js
@@ -5,28 +5,53 @@ const https = require('https')
 
 const now = require('../lib/now')
 
-test('now client', (t) => {
-  let stub = sinon.stub(https, 'request')
+const https_request_options = {
+  hostname: 'api.zeit.co',
+  port: 443,
+  method: 'GET',
+  headers: {
+    'Authorization': 'Bearer: API-TOKEN'
+  }
+}
 
+const stub = sinon.stub(https, 'request')
+
+now.set_token('API-TOKEN')
+
+test('now client - get deployments list', (t) => {
   stub
-    .withArgs({
-      hostname: 'api.zeit.co',
-      path: '/now/deployments',
-      port: 443,
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer: API-TOKEN'
-      }
-    })
+    .withArgs(Object.assign({path: '/now/deployments'}, https_request_options))
     .returns({end: () => {}})
     .callsArgWith(1, {on: (e, cb) => {
       cb(e == 'data' ? '{"deployments": []}' : null)
     }})
 
-  now.set_token('API-TOKEN')
   now.get_deployments((list) => {
     t.equal(list.constructor, Array, 'deployments is an array')
     t.equal(list.length, 0, 'with zero items')
     t.end()
+  })
+})
+
+test('now client - get package.json', (t) => {
+  stub
+    .withArgs(Object.assign({path: '/now/deployments/uid/files'}, https_request_options))
+    .returns({end: () => {}})
+    .callsArgWith(1, {on: (e, cb) => {
+      cb(e == 'data' ? '[{"name": "package.json", "uid": "pkg-uid"}]' : null)
+    }})
+
+  stub
+    .withArgs(Object.assign({path: '/now/deployments/uid/files/pkg-uid'}, https_request_options))
+    .returns({end: () => {}})
+    .callsArgWith(1, {on: (e, cb) => {
+      cb(e == 'data' ? '{"version": "1.1.1"}' : null)
+    }})
+
+  now.get_pkg('uid', (pkg) => {
+    t.equal(pkg.constructor, Object, 'package.json is an object')
+    t.pass(pkg.version, 'with a version')
+    t.end()
+    https.request.restore()
   })
 })
