f5fd31e674b35939418743f534e0709855f7004a
Jim de Beer <jim@vigour.io>
2016-07-30 12:21:20 +0300

    added travis, scripts (npm run watch), and coveralls

--- test/now.js
+++ test/now.js
@@ -3,9 +3,10 @@ const test = require('tape')
 const sinon = require('sinon')
 const https = require('https')
 
-const now = require('../lib/now')
+// allways use lib/index.js as a main
+const now = require('../')
 
-const https_request_options = {
+const httpsRequestOptions = {
   hostname: 'api.zeit.co',
   port: 443,
   method: 'GET',
@@ -22,10 +23,10 @@ test('now client - get deployments list', (t) => {
   stub
     .withArgs(Object.assign({
       path: '/now/deployments'
-    }, https_request_options))
+    }, httpsRequestOptions))
     .returns({end: () => {}})
     .callsArgWith(1, {on: (e, cb) => {
-      cb(e == 'data' && '{"deployments": []}')
+      cb(e === 'data' && '{"deployments": []}')
     }})
 
   now.get_deployments((list) => {
@@ -39,19 +40,19 @@ test('now client - get package.json', (t) => {
   stub
     .withArgs(Object.assign({
       path: '/now/deployments/deployment-uid/files'
-    }, https_request_options))
+    }, httpsRequestOptions))
     .returns({end: () => {}})
     .callsArgWith(1, {on: (e, cb) => {
-      cb(e == 'data' && '[{"name": "package.json", "uid": "pkg-uid"}]')
+      cb(e === 'data' && '[{"name": "package.json", "uid": "pkg-uid"}]')
     }})
 
   stub
     .withArgs(Object.assign({
       path: '/now/deployments/deployment-uid/files/pkg-uid'
-    }, https_request_options))
+    }, httpsRequestOptions))
     .returns({end: () => {}})
     .callsArgWith(1, {on: (e, cb) => {
-      cb(e == 'data' && '{"version": "1.1.1"}')
+      cb(e === 'data' && '{"version": "1.1.1"}')
     }})
 
   now.get_pkg('deployment-uid', (pkg) => {
