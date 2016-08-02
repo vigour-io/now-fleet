be81ee2aa131d8945b7df6e247835d004af8abe2
Mustafa Dokumaci <mustafa@foraa.com>
2016-08-02 04:42:00 +0200

    More ES6

--- test/now.js
+++ test/now.js
@@ -4,7 +4,8 @@ const test = require('tape')
 const sinon = require('sinon')
 const https = require('https')
 
-const now = require('../lib/now')
+const Now = require('../lib/now')
+const now = new Now('API-TOKEN')
 
 const httpsRequestOptions = {
   hostname: 'api.zeit.co',
@@ -17,8 +18,6 @@ const httpsRequestOptions = {
 
 const stub = sinon.stub(https, 'request')
 
-now.setToken('API-TOKEN')
-
 test('now client - get deployments list', t => {
   stub
     .withArgs(Object.assign({
@@ -29,11 +28,12 @@ test('now client - get deployments list', t => {
       cb(e === 'data' && '{"deployments": []}')
     }})
 
-  now.getDeployments(list => {
-    t.equal(list.constructor, Array, 'deployments is an array')
-    t.equal(list.length, 0, 'with zero items')
-    t.end()
-  })
+  now.getDeployments()
+    .then(list => {
+      t.equal(list.constructor, Array, 'deployments is an array')
+      t.equal(list.length, 0, 'with zero items')
+      t.end()
+    })
 })
 
 test('now client - get package.json', t => {
@@ -55,11 +55,12 @@ test('now client - get package.json', t => {
       cb(e === 'data' && '{"version": "1.1.1"}')
     }})
 
-  now.getPkg('deployment-uid', pkg => {
-    t.equal(pkg.constructor, Object, 'package.json is an object')
-    t.ok(pkg.version, 'with a version')
-    t.end()
-  })
+  now.getPkg('deployment-uid')
+    .then(pkg => {
+      t.equal(pkg.constructor, Object, 'package.json is an object')
+      t.ok(pkg.version, 'with a version')
+      t.end()
+    })
 })
 
 test.onFinish(() => {
--- test/npm.js
+++ test/npm.js
@@ -4,39 +4,33 @@ const test = require('tape')
 const sinon = require('sinon')
 const cp = require('child_process')
 
-const npm = require('../lib/npm')
+const Npm = require('../lib/npm')
+const npm = new Npm()
 
 const stub = sinon.stub(cp, 'exec')
 
 test('npm client - get latest version', t => {
   stub
     .withArgs('npm v sample@^1.0.0 version')
-    .returns({
-      stdout: {on: (e, cb) => {
-        cb('\nsample@1.1.1 \'1.1.1\'\nsample@1.2.2 \'1.2.2\'\nsample@1.3.3 \'1.3.3\'\n')
-      }},
-      on: (e, cb) => cb()
-    })
+    .callsArgWith(2, null, '\nsample@1.1.1 \'1.1.1\'\nsample@1.2.2 \'1.2.2\'\nsample@1.3.3 \'1.3.3\'\n')
 
-  npm.getLastVersion('sample@^1.0.0', version => {
-    t.equal(version, '1.3.3', 'latest version is 1.3.3')
-    t.end()
-  })
+  npm.getLastVersion('sample@^1.0.0')
+    .then(version => {
+      t.equal(version.constructor, String, 'latest version is a string')
+      t.equal(version, '1.3.3', 'latest version is 1.3.3')
+      t.end()
+    })
 })
 
 test('npm client - get services', t => {
   stub
     .withArgs('npm v sample@1.3.3 services')
-    .returns({
-      stdout: {on: (e, cb) => {
-        cb('\n{"sample2": "^2.0.0"}\n')
-      }},
-      on: (e, cb) => cb()
-    })
+    .callsArgWith(2, null, '\n{"sample2": "^2.0.0"}\n')
 
-  npm.getServices('sample@1.3.3', services => {
-    t.equal(services.constructor, Object, 'services is an object')
-    t.equal(services.sample2, '^2.0.0', 'sample2@^2.0.0 is a service dependency')
-    t.end()
-  })
+  npm.getServices('sample@1.3.3')
+    .then(services => {
+      t.equal(services.constructor, Object, 'services is an object')
+      t.equal(services.sample2, '^2.0.0', 'sample2@^2.0.0 is a service dependency')
+      t.end()
+    })
 })
