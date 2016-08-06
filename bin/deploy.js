a046eea4bbf1bde04566e3ef89ed2135b6f2ad63
Mustafa Dokumaci <mustafa@foraa.com>
2016-08-06 13:06:53 +0200

    Deploy root service first, others later in parallel

---
+++ bin/deploy.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Services = require('../lib/services')
+const services = new Services()
+
+services.deployAll(process.cwd())
+  .then(() => {
+    console.info('Deployment successful. Services will discover each other soon.')
+  })
+  .catch((error) => {
+    console.error('Deployment failed due to error: %j', error)
+  })
